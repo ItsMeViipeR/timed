dd2d4c9489bcb084020f847d42366dd29aacdc8d
ItsMeVipeR <itsmeviiper@gmail.com>
2026-07-28 19:02:55 +0200

    add until

--- timed.go
+++ timed.go
@@ -31,6 +31,27 @@ func Since(date any) (string, error) {
 	return formatDuration(duration), nil
 }
 
+func Until(date any) (string, error) {
+	var targetTime time.Time
+
+	switch v := date.(type) {
+	default:
+		return "", errors.New("unable to parse date")
+	case time.Time:
+		targetTime = v
+	case string:
+		parsedTime, err := StringToTime(v)
+		if err != nil {
+			return "", err
+		}
+		targetTime = parsedTime
+	}
+
+	duration := time.Until(targetTime)
+
+	return formatUntilDuration(duration), nil
+}
+
 func StringToTime(str string) (time.Time, error) {
 	layout := "2006-01-02 15:04:05"
 
@@ -44,20 +65,46 @@ func StringToTime(str string) (time.Time, error) {
 }
 
 func formatDuration(d time.Duration) string {
-	if d < 0 {
-		return "in the future"
-	}
 	if d < time.Minute {
-		return fmt.Sprintf("%.0f seconds ago", d.Seconds())
+		return fmt.Sprintf("%d seconds ago", int(d.Seconds()))
 	}
 	if d < time.Hour {
-		return fmt.Sprintf("%.0f minutes ago", d.Minutes())
+		return fmt.Sprintf("%d minutes ago", int(d.Minutes()))
 	}
 	if d < 24*time.Hour {
-		return fmt.Sprintf("%.0f hours ago", d.Hours())
+		return fmt.Sprintf("%d hours ago", int(d.Hours()))
 	}
 
 	days := int(d.Hours() / 24)
-
 	return fmt.Sprintf("%d days ago", days)
+}
+
+func formatUntilDuration(d time.Duration) string {
+	if d < 0 {
+		d = -d
+		if d < time.Minute {
+			return fmt.Sprintf("%d seconds ago", int(d.Seconds()))
+		}
+		if d < time.Hour {
+			return fmt.Sprintf("%d minutes ago", int(d.Minutes()))
+		}
+		if d < 24*time.Hour {
+			return fmt.Sprintf("%d hours ago", int(d.Hours()))
+		}
+		days := int((d + time.Hour).Hours() / 24)
+		return fmt.Sprintf("%d days ago", days)
+	}
+
+	if d < time.Minute {
+		return fmt.Sprintf("In %d seconds", int(d.Seconds()))
+	}
+	if d < time.Hour {
+		return fmt.Sprintf("In %d minutes", int(d.Minutes()))
+	}
+	if d < 24*time.Hour {
+		return fmt.Sprintf("In %d hours", int(d.Hours()))
+	}
+
+	days := int((d + time.Hour).Hours() / 24)
+	return fmt.Sprintf("In %d days", days)
 }
